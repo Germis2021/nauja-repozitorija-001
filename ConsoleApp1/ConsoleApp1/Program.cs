61fc37ec866349f97c230963ac14f3755c561e38
Germis2021 <89936809+Germis2021@users.noreply.github.com>
2021-09-06 17:00:48 +0300

    parasiau programa

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char A;
+
+            Console.WriteLine("Raides nuo a - z  ");
+            for (A = 'a'; A <= 'z'; A++)
+            {
+
+                Console.WriteLine(A);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
